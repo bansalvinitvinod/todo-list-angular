613ab1fa0633b2da442a33c4732793b196a1f853
Vinit Bansal <vinitbansal1998@gmail.com>
2021-09-28 18:48:40 +0530

    Strict Initialisation Turned On

--- src/app/MyComponents/todo-item/todo-item.component.ts
+++ src/app/MyComponents/todo-item/todo-item.component.ts
@@ -8,7 +8,9 @@ import { Todo } from 'src/app/MyModels/todo';
 })
 export class TodoItemComponent implements OnInit {
 
-  @Input() todo: Todo;
+  // ! after variable name is used to tell the compiler that we are sure that the value is not undefined or null of that variable
+  // This is done to avoid strict initialisation
+  @Input() todo!: Todo;
   @Output() todoDelete: EventEmitter<Todo> = new EventEmitter();
   constructor() { }
 
